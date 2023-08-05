7c743ce87940f60c4b844c5c29eb76918632ac93
= <=>
2023-08-05 22:25:26 +1200

    create movie card component

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-white shadow-lg rounded-lg overflow-hidden">
+    <img
+      :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path"
+      alt="Movie Poster"
+      class="w-full h-auto"
+    />
+    <div class="p-4">
+      <h2 class="text-xl font-semibold">{{ movie.original_title }}</h2>
+      <p class="text-gray-600">{{ formattedDate }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { format, parseISO } from 'date-fns'
+
+const props = defineProps({
+  movie: Object,
+})
+
+const parsedDate = parseISO(props.movie.release_date)
+const formattedDate = formatDate(parsedDate)
+
+function formatDate(date) {
+  return format(date, 'MMMM d, yyyy')
+}
+</script>
