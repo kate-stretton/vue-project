70f454f1d44a3e29d2240fe4b443bd053b34e638
= <=>
2023-08-05 20:10:11 +1200

    get a list of movies via axios

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+const allMovies = ref([])
+
+const getMovies = () => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=136d55d9c6878da748d19b6aa4870c86&`
+    )
+    .then((response) => {
+      allMovies.value = response.data.results
+    })
+}
+
+onMounted(() => {
+  getMovies()
+})
+</script>
 
 <template>
-  <div>Hello world</div>
+  <div>Movies</div>
+  <div>
+    <ul>
+      <li v-for="movie in allMovies" :key="movie.id">
+        {{ movie.original_title }}
+      </li>
+    </ul>
+  </div>
 </template>
